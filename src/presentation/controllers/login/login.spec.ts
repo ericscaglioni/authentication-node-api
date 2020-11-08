07320bd13308d1871fe9225a5c5a0b91311f1594
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-11-08 16:23:25 -0300

    test: ensure LoginController calls Authentication with correct data

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,13 +1,26 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
 import { LoginController } from './login'
+
+const makeAuthenticator = (): Authentication => {
+  class AuthenticatorStub implements Authentication {
+    async authenticate (authentication: AuthenticationModel): Promise<string> {
+      return new Promise(resolve => resolve('any_token'))
+    }
+  }
+  return new AuthenticatorStub()
+}
 
 interface SutTypes {
   sut: LoginController
+  authenticatorStub: Authentication
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoginController()
+  const authenticatorStub = makeAuthenticator()
+  const sut = new LoginController(authenticatorStub)
   return {
-    sut
+    sut,
+    authenticatorStub
   }
 }
 
@@ -28,11 +41,29 @@ describe('Login Controller', () => {
     const { sut } = makeSut()
     const httpRequest: any = {
       body: {
-        email: 'any_email'
+        email: 'any_email@email.com'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('password'))
+  })
+
+  test('Should call Authenticator with correct data', async () => {
+    const { sut, authenticatorStub } = makeSut()
+
+    const authenticatSpy = jest.spyOn(authenticatorStub, 'authenticate')
+
+    const httpRequest: any = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(authenticatSpy).toHaveBeenCalledWith({
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
   })
 })
