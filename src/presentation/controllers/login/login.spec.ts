9dd03cb4123b83ea4a132cc6c60656b95cb2dddd
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-11-08 16:01:01 -0300

    test: ensure LoginController returns 400 when email is not provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,15 @@
+import { LoginController } from './login'
+
+describe('Login Controller', () => {
+  test('Should return status code 400 if email is not provided', async () => {
+    const sut = new LoginController()
+    const httpRequest: any = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('email'))
+  })
+})
