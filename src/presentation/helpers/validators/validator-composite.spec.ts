67a8c10dc4902df6c30512db357268d9e613fedb
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-11-09 09:33:42 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/presentation/helpers/validators/validator-composite.spec.ts
+++ src/presentation/helpers/validators/validator-composite.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from './../../errors'
+import { MissingParamError, InvalidParamError } from './../../errors'
 import { Validator } from './../../protocols'
 import { ValidatorComposite } from './validator-composite'
 
@@ -33,5 +33,15 @@ describe('Validator Composite', () => {
 
     const error = sut.validate({ field: 'any_field' })
     expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('Should return the first error if more than one validation fails', () => {
+    const { sut, validatorStubs } = makeSut()
+
+    jest.spyOn(validatorStubs[0], 'validate').mockReturnValueOnce(new InvalidParamError('any_field'))
+    jest.spyOn(validatorStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toEqual(new InvalidParamError('any_field'))
   })
 })
