4c6d5f3c62e509b0d24db590511950f9b8ae68e7
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-11-09 09:12:59 -0300

    feat: ensure RequiredFieldsValidator returns a MissingParamError if validation fails

---
+++ src/presentation/helpers/validators/required-fields-validator.ts
@@ -0,0 +1,18 @@
+import { MissingParamError } from './../../errors'
+import { Validator } from './../../protocols'
+
+export class RequiredFieldsValidator implements Validator {
+  private readonly fieldNames: string[]
+
+  constructor (fieldNames: string[]) {
+    this.fieldNames = fieldNames
+  }
+
+  validate (input: any): Error {
+    for (const fieldName of this.fieldNames) {
+      if (!input[fieldName]) {
+        return new MissingParamError(fieldName)
+      }
+    }
+  }
+}
